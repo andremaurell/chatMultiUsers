5c41ba3d85adf4c829f0362b0cde4670a19000a3
andremaurell <andremaurell3@gmail.com>
2023-06-07 13:12:00 -0300

    a

--- server/index.js
+++ server/index.js
@@ -26,8 +26,8 @@
 
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'http://192.168.43.58:5173'}})
-const PORT = 3001
+const io = require('socket.io')(server, {cors: {origin: '*'}})
+const PORT = process.env.PORT || 3001
 
 io.on('connection', socket => {
   console.log('Usuário conectado!', socket.id);
