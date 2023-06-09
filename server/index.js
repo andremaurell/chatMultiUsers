b5e32b4593900d0384a404967cba93584f2f50d5
andremaurell <andremaurell3@gmail.com>
2023-06-08 21:06:27 -0300

    oap

--- server/index.js
+++ server/index.js
@@ -1,29 +1,3 @@
-//const cluster = require('cluster');
-//const numCPUs = require('os').cpus().length;
-
-
-//if (cluster.isPrimary) {
-//  console.log(`Primary ${process.pid} is running`);
-
-  //Crie um worker para cada CPU disponível
- // for (let i = 0; i < numCPUs; i++) {
-  //  cluster.fork();
- // }
-
- // cluster.on('exit', (worker, code, signal) => {
- //   console.log(`Worker ${worker.process.pid} died. Creating a new worker.`);
- //   cluster.fork();
- // });
-
-  // Escute eventos de fork de novos workers
- // cluster.on('fork', (worker) => {
- //   console.log(`Worker ${worker.process.pid} was forked`);
- // });
-
-
-//} else {
-
-
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {cors: {origin: '*'}})
@@ -53,4 +27,3 @@ io.on('connection', socket => {
 }) 
 server.listen(PORT, () => 
 console.log(`is running on port ${PORT}`))
-//}
