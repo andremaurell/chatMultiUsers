86beb6d4838a580c68c308773493a792cf256e6e
Andre Maurell <andremaurell3@furg.br>
2023-06-08 22:15:09 -0300

    pc-desktop

--- server/index.js
+++ server/index.js
@@ -1,40 +1,7 @@
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-
-
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`);
-
-  // Crie um worker para cada CPU disponível
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died. Creating a new worker.`);
-    cluster.fork();
-  });
-
-  // Escute eventos de fork de novos workers
-  cluster.on('fork', (worker) => {
-    console.log(`Worker ${worker.process.pid} was forked`);
-  });
-
-
-} else {
-
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'http://192.168.1.12:5173'}})
-const PORT = 3001
-
-//const redisAdapter = require('socket.io-redis');
-
-// const redisURL = 'redis://sua-url-redis-aqui';
-// const adapter = redisAdapter(redisURL);
-// io.adapter(adapter);
-
-
+const io = require('socket.io')(server, {cors: {origin: '*'}})
+const PORT = process.env.PORT
 
 io.on('connection', socket => {
   console.log('Usuário conectado!', socket.id);
@@ -59,4 +26,3 @@ io.on('connection', socket => {
 
 server.listen(PORT, () => 
 console.log(`Worker ${process.pid} is running on port ${PORT}`))
-}
